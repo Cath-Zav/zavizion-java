45fe51c3bc551a35e6a7b150245c36d28afb8fb0
Cath-Zav <cathzavizion@gmail.com~>
2025-08-01 00:56:57 +0200

    add body and Exceptions to methods sum(), subtract(), multiply(), divide()

--- src/main/java/by/itacademy/zavizion/Calculator.java
+++ src/main/java/by/itacademy/zavizion/Calculator.java
@@ -1,4 +1,48 @@
 package by.itacademy.zavizion;
 
-public class Calculator {
+public class Calculator implements ICalculator {
+
+    @Override
+    public int sum(int a, int b) {
+        int result = 0;
+        try {
+            result = Math.addExact(a, b);
+        } catch(ArithmeticException e) {
+            System.out.println("Ошибка переполнения при сложении");
+        }
+        return result;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        int result = 0;
+        try {
+            result = Math.subtractExact(a, b);
+        } catch (ArithmeticException e){
+            System.out.println("Ошибка переполнения при  вычитании");
+        }
+        return result;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        int result = 0;
+        try {
+            result = Math.multiplyExact(a, b);
+        } catch(Exception e) {
+            System.out.println("Ошибка переполнения при умножении");
+        }
+        return result;
+    }
+
+    @Override
+    public double divide(int a, int b) {
+        double result = 0;
+        try{
+            result = a / b;
+        } catch(Exception e) {
+            System.out.println("Ошибка деления на ноль");
+        }
+        return result;
+    }
 }
