c0582eb399860a8b18f29a7513a937b5da7945df
Cath-Zav <cathzavizion@gmail.com~>
2025-08-08 01:00:27 +0200

    add pizzetempo xpath

---
+++ src/main/java/by/pizzatempo/LoginPageLocator.java
@@ -0,0 +1,12 @@
+package by.pizzatempo;
+
+public class LoginPageLocator {
+    public static final String LOGIN_TITLE = "//form[@class='auth']/div[@class='title']";
+    public static final String LOGIN_INPUT_EMAIL = "//input[@name='astroauth_login']";
+    public static final String LOGIN_INPUT_PASSWORD = "//input[@name='astroauth_pass']";
+    public static final String LOGIN_INPUT_SUBMIT = "//input[@name='astroauth_submit']";
+    public static final String LOGIN_REGISTRATION_LINK = "//a[@href='https://www.pizzatempo.by/registration/']";
+    public static final String LOGIN_FORGET_PASSWORD_LINK = "//a[@href='https://www.pizzatempo.by/registration/forgot/']";
+
+    ;
+}
