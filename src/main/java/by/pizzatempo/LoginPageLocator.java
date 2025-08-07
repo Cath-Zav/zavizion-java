541255b0308fff42f303f13f67dc0ca213c36d09
Cath-Zav <cathzavizion@gmail.com~>
2025-08-08 01:02:52 +0200

    refactor

--- src/main/java/by/pizzatempo/LoginPageLocator.java
+++ src/main/java/by/pizzatempo/LoginPageLocator.java
@@ -7,6 +7,4 @@ public class LoginPageLocator {
     public static final String LOGIN_INPUT_SUBMIT = "//input[@name='astroauth_submit']";
     public static final String LOGIN_REGISTRATION_LINK = "//a[@href='https://www.pizzatempo.by/registration/']";
     public static final String LOGIN_FORGET_PASSWORD_LINK = "//a[@href='https://www.pizzatempo.by/registration/forgot/']";
-
-    ;
 }
