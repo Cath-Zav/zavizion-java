5c723936f70753ff2bb7cdbf8b03334e16ded58f
Cath-Zav <cathzavizion@gmail.com~>
2025-08-08 00:29:42 +0200

    add dodopizza xpath for Home Page and Login

---
+++ src/main/java/by/dodopizza/HomePageLocator.java
@@ -0,0 +1,6 @@
+package by.dodopizza;
+
+public class HomePageLocator {
+    public static final String BUTTON_HEADERLOGIN="//button[@data-testid='header_login']";
+    public static final String BUTTON_CART="//button[@data-testid='navigation__cart']";
+}
